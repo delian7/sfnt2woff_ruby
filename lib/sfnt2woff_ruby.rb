57bf97fd29607bc14e6276dc8f6d64011a923c7e
Delian Petrov <petrovd@uci.edu>
2016-02-11 13:50:38 -0800

    change the method name to convert_to_woff

--- lib/sfnt2woff_ruby.rb
+++ lib/sfnt2woff_ruby.rb
@@ -1,5 +1,5 @@
 class Sfnt2Woff
-  def self.hi(font_path)
+  def self.convert_to_woff(font_path)
     sfnt2woff_path = File.join(File.dirname(__FILE__),'../bin/sfnt2woff')
     file = system "#{sfnt2woff_path} #{font_path}"
   end
